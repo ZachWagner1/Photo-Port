24bf87d0c3e27a8a72ac94fca95b5c24ae1ddab0
ZachWagner1 <zwag14@yahoo.com>
2020-10-01 18:16:33 -0500

    contact form is a controlled component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import Contact from './components/Contact'
 
 function App() {
   const [categories] = useState([
@@ -25,8 +26,9 @@ function App() {
       ></Nav>
       <main>
         <div>
+          <Contact />
           <Gallery currentCategory={currentCategory}/>
-          <About/>
+          <About />
         </div>
       </main>
     </div>
