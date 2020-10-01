2c93be95a5034012609db38d276148798daa68df
ZachWagner1 <zwag14@yahoo.com>
2020-10-01 18:59:55 -0500

    contact tests

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
-import Contact from './components/Contact'
+import ContactForm from './components/Contact'
 
 function App() {
   const [categories] = useState([
@@ -17,18 +17,30 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [ contactSelected, setContactSelected ] = useState(false)
+
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
         <div>
-          <Contact />
+          {/* <Contact />
+           <About /> */}
           <Gallery currentCategory={currentCategory}/>
-          <About />
+          {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory} />
+            <About />
+          </>
+        ) : (
+          <ContactForm />
+        )}
         </div>
       </main>
     </div>
